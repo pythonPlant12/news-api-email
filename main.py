9746576ca2cc351ea8f9c510fe445cd7ce9f8b9b
Nikita Lutsai <105123083+nikitalutsai@users.noreply.github.com>
2023-01-15 14:44:35 +0100

    Specify language, limit news, add subject, control category

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests
 from email_send import send_message
 api_key = "92d25a75f06346ad8a38bcb53577bdf8"
-url = "https://newsapi.org/v2/everything?domains=wsj.com&apiKey=92d25a75f06346ad8a38bcb53577bdf8"
+url = "https://newsapi.org/v2/everything?" \
+      "domains=wsj.com" \
+      "&apiKey=92d25a75f06346ad8a38bcb53577bdf8" \
+      "&language=en"
 
 # Make request
 request = requests.get(url)
 # Get dictionary
 content = request.json()
+
 
 # Define a function to return title, body and creator in a single f-string
 def news_message(title, body, creator):
@@ -19,9 +23,9 @@ def news_message(title, body, creator):
     return message1
 
 
-message_list = ""
+message_list = "Subject: Daily News"
 # Access the article titles and description and iterate over each, creating a large string
-for article in content["articles"]:
+for article in content["articles"][:20]:
     if article["title"] is not None:
         message_list = message_list + (news_message(article["title"], article["content"], article["url"]))
 # Call function from email_send where the string is sent to the receiver
